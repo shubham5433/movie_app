9b2cfbddb9a53e108f0679274df8d0a546f830ee
shubhamsingh5433@gmail.com <shubhamsingh5433@gmail.com>
2022-12-20 22:24:36 +0530

    first

---
+++ src/components/searchMovies/searchMovies.js
@@ -0,0 +1,59 @@
+import React ,{ useState } from "react";
+import "./searchMovies.css"
+import Cards from "../card/card"
+
+
+
+export default function SearchMovies(){
+
+   //states- input query,movies
+   //useState is used to track 
+    const [query,setQuery] = useState("");
+    const [movies,setMovies]=useState([]);
+    
+    const searchMovies =async(e)=>{
+        e.preventDefault();
+       // console.log("submit");
+        
+        const url=`https://api.themoviedb.org/3/search/movie?api_key=04c35731a5ee918f014970082a0088b1&language=en-US&query=${query}&include_adult=false`;
+        
+
+        try{
+            const res = await fetch(url);
+            const data= await res.json();
+            //console.log(data.results);
+            setMovies(data.results);
+        }catch(err){
+            console.error(err);
+        }
+    }
+
+    return(
+        <>      
+        <form onSubmit={searchMovies}>
+        <input type="text" 
+        name="query" 
+        placeholder="Search here"
+        value={query} 
+        onChange={(e)=>setQuery(e.target.value)}
+        />
+        <button type="submit" >search</button>
+        </form>
+         
+        <div className="movie__list">
+            
+            <h2 className="list__title">{(query).toUpperCase()}</h2>
+            <div className="list__cards">
+                {
+                    movies.filter(movie=>movie.poster_path).map(movie => (
+                        <Cards movie={movie} />
+                    ))
+                }
+            </div>
+        </div>
+        
+        
+
+        </>
+    )
+}
